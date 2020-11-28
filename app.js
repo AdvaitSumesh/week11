5cbec2b26a6e844e0b9e4124607563f55f42dd67
AdvaitSumesh <70094587+AdvaitSumesh@users.noreply.github.com>
2020-11-29 00:16:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,168 @@
+var cartValue = document.getElementById("cart-value");
+var cartButton=document.getElementById("cart");
+
+
+var items=[
+ {
+    name:"This was our pact",
+    quantity:0,
+    dollars: 7,
+    cents:49 ,
+}
+
+// console.log(book1.name)
+
+ = {
+    name:"The famous five" ,
+    quantity:0 ,
+    dollars:4 ,
+    cents:59 ,
+},
+
+! { 
+    name:"Matlida",
+    quantity:0 ,
+    dollars:6 ,
+    cents:80 ,
+},
+
+ !{
+    name:"Harry Potter",
+    quantity:0 ,
+    dollars:10 ,
+    cents:0 ,
+},
+
+{
+    name:"For Young Readers",
+    quantity:0 ,
+    dollars:7 ,
+    cents:29 ,
+},
+
+{
+   name:"Wimpy Kid - DIY",
+    quantity:0 ,
+    dollars:4 ,
+    cents:99 ,
+},
+{
+    name:"Dart Board" ,
+    quantity:0 ,
+    dollars:17 ,
+    cents:49 ,
+},
+{
+    name:"Connect 4",
+    quantity:0 ,
+    dollars:19 ,
+    cents:99 ,
+},
+{
+    name:"Jenga",
+    quantity:0 ,
+    dollars:20 ,
+    cents:99 ,
+},
+{
+    name:"Monopoly",
+    quantity:0 ,
+    dollars:19 ,
+    cents:49 ,
+}
+,
+{
+    name:"Bookmarks" ,
+    quantity:0 ,
+    dollars:12 ,
+    cents:49 ,
+},
+{
+    name:"Birthday card" ,
+    quantity: 0 ,
+    dollars: 19 ,
+    cents: 99 ,
+},
+{
+    name:"Stuffed toys" ,
+    quantity:0 ,
+    dollars:15 ,
+    cents:99 ,
+},
+{
+    name:"Dream catcher drawing" ,
+    quantity:0 ,
+    dollars:18 ,
+    cents:49 ,
+}
+
+
+
+
+
+
+function updatecart(){
+    let cart = 0;
+    for (index = 0; index < items.length; index++){
+        cart = cart + items[index].quantity;
+    }
+    cartValue.innerHTML = cart;
+}
+
+// function updatecart(){
+//     cart=
+//     book1.quantity +
+//     book2.quantity +
+//     book3.quantity +
+//     book4.quantity +
+//     book5.quantity +
+//     book6.quantity +
+//     game1.quantity +
+//     game2.quantity +
+//     game3.quantity +
+//     game4.quantity +
+//     craft1.quantity +
+//     craft2.quantity +
+//     craft3.quantity +
+//     craft4.quantity +
+//     cartValue.innerHTML =  cart;
+// }
+
+for (let i = 0; i < addButtons.length; i++){
+addButtons[i].oneclick(e)=> {
+    items[i].quantity++;
+    updatecart();
+};
+}
+
+var finalDollars = 0;
+var finalCents = 0;
+
+function updatePrice() {
+    let totalPriceInCents = 0;
+
+    for (index = 0; index < items.length; index++) {
+        totalPriceInCents=totalPriceInCents+items[index].quantity*(items[index].dollars*100+items[index].cents);
+    }
+    finalDollars = Math.floor(totalPriceInCents  / 100);
+finalCents =totalPriceInCents % 100;
+}
+
+var whatsappLink = 
+"https://api.whatsapp.com/send?phone=919000000000text=Order%20details";
+
+function updateWhatsappLink() {
+    for (let index = 0; index < items.length; index++){
+        if (items[index].quantity !=0){
+            whatsappLink += "%0A" + items[index].name + "%20" + items[index].quantity;
+        }
+    }
+}
+whatsappLink+="%0A"+"Total%20Price:%20$"+finalDollars+"%20"+finalCents+"C";}
+
+
+cartButton.oneclick = () => {
+updatePrice();
+updateWhatsappLink();
+window.open(whatsappLink, "blank");
+    };
